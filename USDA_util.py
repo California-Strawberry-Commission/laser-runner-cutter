54b711f673c07d2c7f3f5f1e168d608c537d7ff0
Bobby Vinson <rvinson@calstrawberry.org>
2023-08-30 12:13:06 -0700

    Additional USDA utility functionality

--- USDA_util.py
+++ USDA_util.py
@@ -23,6 +23,8 @@ def list_ports():
             non_working_ports.append(dev_port)
         else:
             is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
             if is_reading:
                 working_ports.append(dev_port)
             else:
@@ -30,9 +32,53 @@ def list_ports():
         dev_port +=1
     return available_ports,working_ports,non_working_ports
 
+def safety_check(): 
+    layout = [
+        [sg.Text("This will enable a high power laser,\n Ensure safety requirements are followed.")],
+        [sg.Button("Enable Laser"), sg.Button("Cancel")]
+    ]
+    
+    window = sg.Window("Safety Check", layout, finalize=True)
+    
+    while True:
+        event, _ = window.read(timeout=50)
+        
+        if event in (sg.WIN_CLOSED, "Cancel"):
+            window.close()
+            return False
+        elif event == "Enable Laser":
+            window.close()
+            return True
+        
+       
+
+def idle_check(): 
+    start_time = time.time()
+    layout = [
+        [sg.Text("No user input detected.")],
+        [sg.Button("Continue Operating"), sg.Button("Disable Laser")]
+    ]
+    
+    window = sg.Window("Idle Check", layout, finalize=True)
+    
+    
+    while time.time() - start_time < 10:
+        event, _ = window.read(timeout=50)
+        
+        if event in (sg.WIN_CLOSED, "Disable Laser"):
+            window.close()
+            return False
+        elif event == "Continue Operating":
+            window.close()
+            return True
+    window.close()
+    return False
+        
+
 class OpMode(Enum): 
-    TARGET = 0 
-    FIRE = 1
+    LASER_OFF = 0
+    TARGET = 1
+    FIRE = 2
 
 def laser_left(step_size): 
     laser_point[0]=max(0, laser_point[0]-step_size)
@@ -76,45 +122,70 @@ laser.initialize()
 #Information about laser location 
 laser_point = np.array(laser.frame_shape)/2
 
-pwm_max = 100
+#pwm_max = 100
 # Define the GUI layout
 slider_layout = [
    [sg.Text("Red:"), sg.Slider(range=(0, 255), orientation="h", size=(10, 20), key="-SLIDER_R-", default_value=0)],
    [sg.Text("Blue:"), sg.Slider(range=(0, 255), orientation="h", size=(10, 20), key="-SLIDER_B-", default_value=255)],
    [sg.Text("Green:"), sg.Slider(range=(0, 255), orientation="h", size=(10, 20), key="-SLIDER_G-", default_value=0)],
-   [sg.Text("Intensity:"), sg.Slider(range=(0, 255), orientation="h", size=(10, 20), key="-SLIDER_I-", default_value=255)],
-   [sg.Text("Pulse Width Modulation:"), sg.Slider(range=(1, pwm_max), orientation="h", size=(10, 20), key="-SLIDER_PWM-", default_value=pwm_max)]
+   #Disabled for now
+   #[sg.Text("Intensity:"), sg.Slider(range=(0, 255), orientation="h", size=(10, 20), key="-SLIDER_I-", default_value=255)],
+   #[sg.Text("Pulse Width Modulation:"), sg.Slider(range=(1, pwm_max), orientation="h", size=(10, 20), key="-SLIDER_PWM-", default_value=pwm_max)]
+   [sg.Button("Exit")],
 ]
 time_layout = [
+    [sg.Text("Camera Number:"), sg.Combo(working,  expand_x=True, enable_events=True,  readonly=False, key='-CAM_NUM-', default_value=0)],
     [sg.Text("On Time (Seconds)"), sg.Text(size=(10,1), key="-ON_TIME-")],
     [sg.InputText("1", key="-TIME_INPUT-", size=(20, 1))],
-    [sg.Button("Start Test")]
+    [sg.Button("Start Test")],
+    [sg.Text("Laser Off", key='-Laser State-', font=("Helvetica", 14))]
 ]
 setup_layout = [
+    [sg.Text("Laser Position Control, directional buttons can also be used")], 
+    [sg.Button("LEFT"), sg.Button("UP"), sg.Button("DOWN"), sg.Button("RIGHT")],
     [sg.Text("Tracking Laser Step Size"), sg.Slider(range=(1,20), orientation="h", size=(10, 20), key="-SLIDER_RESOLUTION-", default_value=5)],
-    [sg.Text("Camera Number:"), sg.Combo(working, font=('Arial Bold', 14),  expand_x=True, enable_events=True,  readonly=False, key='-CAM_NUM-', default_value=0)],
+    [sg.Button("Enable Tracking Laser")],
+]
+safety_layout = [
+    [sg.Image('LaserWarning.png', expand_x=True, expand_y=True)],
+    [sg.Text("WARNING - High Power Laser", font=("Helvetica", 14))],
+    [sg.Text("Make sure no one is in the path of the laser. \nNever look directly at the laser without eye protection. \nMake sure no unintended materials will be hit by the laser. \nTurn the laser off completely after us.")],
 ]
 
 layout = [
     [
-        [sg.Image(filename="", key="-IMAGE-")],
+        [sg.Image(filename="", key="-IMAGE-"), sg.Column(safety_layout)],
         [   
             sg.Column(setup_layout), 
             sg.Column(slider_layout), 
             sg.Column(time_layout),
         ], 
-        [sg.Button("Exit")]
     ]
 ]
 
 # Create the GUI window
 window = sg.Window("Webcam GUI", layout, return_keyboard_events=True, finalize=True)
+laser_safety_conf = False
 
-mode = OpMode.TARGET
+mode = OpMode.LASER_OFF
+last_action_ts = None
+default_timeout = 60
 start_ts = time.time()
 while True:
-    event, values = window.read(timeout=20)
-
+    if last_action_ts: 
+        if time.time() - last_action_ts>default_timeout: 
+            if not idle_check(): 
+                laser_safety_conf = False
+                mode = OpMode.LASER_OFF
+                laser.sendEmpty(x=laser_point[0], y=laser_point[1])
+                last_action_ts = None
+            else: 
+                last_action_ts = time.time() 
+    
+    event, values = window.read(timeout=50)
+    if not event == "__TIMEOUT__":
+        last_action_ts = time.time() 
+    
     if event == sg.WINDOW_CLOSED or event == "Exit":
         break
 
@@ -134,32 +205,59 @@ while True:
         curr_ts = time.time()
         if curr_ts - start_ts > on_time: 
             laser.sendEmpty(x=laser_point[0], y=laser_point[1])
-            mode=OpMode.TARGET
+            last_action_ts = time.time()
+            mode=OpMode.LASER_OFF
+            window['-Laser State-'].update("Test Completed")
         continue
 
     # Check for key presses using PySimpleGUI's built-in key event handling
     if event in keymapping.keys(): 
         step_size = int(values['-SLIDER_RESOLUTION-'])
         keymapping[event](step_size)  
+    if event == "UP": 
+        step_size = int(values['-SLIDER_RESOLUTION-'])
+        laser_up(step_size)
+    if event == "DOWN": 
+        step_size = int(values['-SLIDER_RESOLUTION-'])
+        laser_down(step_size)
+    if event == "RIGHT": 
+        step_size = int(values['-SLIDER_RESOLUTION-'])
+        laser_right(step_size)
+    if event == "LEFT": 
+        step_size = int(values['-SLIDER_RESOLUTION-'])
+        laser_left(step_size)
+
+    if event == "Enable Tracking Laser": 
+        if not laser_safety_conf: 
+            laser_safety_conf = safety_check()
+        if laser_safety_conf: 
+            laser_on_time = time.time()
+            mode = OpMode.TARGET
+            window['-Laser State-'].update("Tracking Laser On")
 
     if event == "Start Test": 
-        laser.sendEmpty(x=laser_point[0], y=laser_point[1])
-        on_time = float(values["-TIME_INPUT-"])
-        color[0] = int(values['-SLIDER_R-'])
-        color[1] = int(values['-SLIDER_G-'])
-        color[2] = int(values['-SLIDER_B-'])
-        intensity = int(values['-SLIDER_I-'])
-        pwm = int(values['-SLIDER_PWM-'])
-        start_ts = time.time()
-        pad = (pwm_max-pwm)
-        laser.add_point(laser_point, color, pad=pad, intensity=intensity)
-        mode=OpMode.FIRE
-        laser.sendFrame()
+        if not laser_safety_conf: 
+            laser_safety_conf = safety_check()
+        if laser_safety_conf: 
+            window['-Laser State-'].update("Test Ongoing")
+            laser.sendEmpty(x=laser_point[0], y=laser_point[1])
+            on_time = float(values["-TIME_INPUT-"])
+            color[0] = int(values['-SLIDER_R-'])
+            color[1] = int(values['-SLIDER_G-'])
+            color[2] = int(values['-SLIDER_B-'])
+            #disabled for now
+            #intensity = int(values['-SLIDER_I-'])
+            #pwm = int(values['-SLIDER_PWM-'])
+            #pad = (pwm_max-pwm)
+            pad = 0
+            start_ts = time.time()
+            laser.add_point(laser_point, color, pad=pad, intensity=intensity)
+            mode=OpMode.FIRE
+            laser.sendFrame()
 
     if mode == OpMode.TARGET: 
-        laser.add_point(laser_point, (7, 0, 0), pad=0, intensity=1)
+        laser.add_point(laser_point, (10, 0, 0), pad=0, intensity=1)
         laser.sendFrame()
-   
 
     focus =  window.find_element_with_focus()
     if focus: 
