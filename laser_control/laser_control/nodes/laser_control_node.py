006aadcd7972815fbe916732c8c1d3513bcc7e4e
Genki Kondo <kondo.genki@gmail.com>
2023-10-18 14:47:19 -0700

    Create laser_control_interfaces package

--- laser_control/laser_control/nodes/laser_control_node.py
+++ laser_control/laser_control/nodes/laser_control_node.py
@@ -5,25 +5,49 @@ from ament_index_python.packages import get_package_share_directory
 from rclpy.node import Node
 
 from laser_control.laser_dac import EtherDreamDAC, HeliosDAC
+from laser_control_interfaces.srv import ListConnectedDacs
+from laser_control_interfaces.msg import Dac
 
 
 class LaserControlNode(Node):
     def __init__(self):
         super().__init__("laser_control_node")
         self.logger = self.get_logger()
+        self.srv = self.create_service(
+            ListConnectedDacs, "list_connected_dacs", self.list_connected_dacs_callback
+        )
 
         include_dir = os.path.join(
             get_package_share_directory("laser_control"), "include"
         )
-        ether_dream = EtherDreamDAC(os.path.join(include_dir, "libEtherDream.so"))
         helios = HeliosDAC(os.path.join(include_dir, "libHeliosDacAPI.so"))
+        ether_dream = EtherDreamDAC(os.path.join(include_dir, "libEtherDream.so"))
 
-        num_ether_dream_dacs = ether_dream.initialize()
+        # Initialize DAC interfaces
         num_helios_dacs = helios.initialize()
+        num_ether_dream_dacs = ether_dream.initialize()
+
+        # Construct DAC list
+        self.dac_list = []
+        for i in range(0, num_helios_dacs):
+            dac = Dac()
+            dac.type = "helios"
+            dac.name = f"Helios {i}"
+            self.dac_list.append(dac)
+
+        for i in range(0, num_ether_dream_dacs):
+            dac = Dac()
+            dac.type = "ether_dream"
+            dac.name = f"Ether Dream {i}"
+            self.dac_list.append(dac)
 
         self.logger.info(
             f"Found {num_ether_dream_dacs} Ether Dream and {num_helios_dacs} Helios DACs"
         )
+
+    def list_connected_dacs_callback(self, request, response):
+        response.dacs = self.dac_list
+        return response
 
 
 def main(args=None):
