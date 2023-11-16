8dbde5070be193dc647406a834e01c417a4cecd3
Bobby Vinson <rvinson@calstrawberry.com>
2023-11-16 14:24:17 -0800

    Create ml_model package for use in ros structure

---
+++ ml_model/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+from glob import glob
+
+
+package_name = "ml_model"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(),
+    data_files=[("data_store/weights", glob("data_store/weights/*"))],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="bobby",
+    maintainer_email="rvinson@calstrawberry.org",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=[],
+    entry_points={},
+)
