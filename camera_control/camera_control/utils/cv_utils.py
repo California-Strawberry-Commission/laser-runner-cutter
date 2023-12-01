8ac1905639204537ec1be41b9778a39ba808f567
Bobby Vinson <rvinson@calstrawberry.com>
2023-11-30 23:32:04 -0800

    Update camera control node to use no ML_Model package

--- camera_control/camera_control/utils/cv_utils.py
+++ camera_control/camera_control/utils/cv_utils.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-from shapely import Polygon
 
 
 def draw_laser(debug_frame, laser_list):
@@ -29,28 +27,3 @@ def draw_runners(debug_frame, runner_list):
             markerSize=20,
         )
     return debug_frame
-
-
-def detect_runners(model, frames):
-    image = np.asanyarray(frames["color"].get_data())
-    res = model(image)
-    point_list = []
-    if res[0].masks:
-        for cords in res[0].masks.xy:
-            polygon = Polygon(cords)
-            point_list.append((polygon.centroid.x, polygon.centroid.y))
-    return point_list
-
-
-def detect_lasers(model, frames):
-    point_list = []
-    image = np.asanyarray(frames["color"].get_data())
-    res = model(image)
-    if len(res) <= 0 or not res[0].boxes or len(res[0].boxes.xywh) <= 0:
-        return point_list
-
-    for box in res[0].boxes.xywh:
-        box_np = box.numpy().astype(float)
-        point_list.append((box_np[0], box_np[1]))
-
-    return point_list
