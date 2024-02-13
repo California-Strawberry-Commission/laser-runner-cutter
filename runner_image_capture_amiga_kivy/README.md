db00cfd49b4c9e566ac27798c5d492b022740e7f
Genki Kondo <kondo.genki@gmail.com>
2024-02-13 13:53:13 -0800

    [image capture amiga app] Tweak entry script and add README

---
+++ runner_image_capture_amiga_kivy/README.md
@@ -0,0 +1,17 @@
+# Runner Image Capture Amiga App
+
+An application deployable to the Amiga brain, written using Kivy.
+
+## Run from the command line
+
+    cd runner_image_capture_amiga_kivy
+    ./entry.sh -l
+
+## Install and run on Amiga brain
+
+SSH into the Amiga brain and clone the repo there. Then,
+
+    cd runner_image_capture_amiga_kivy
+    ./install.sh
+
+You should now see the app on the brain screen. Click on it to launch it.
--- runner_image_capture_amiga_kivy/entry.sh
+++ runner_image_capture_amiga_kivy/entry.sh
@@ -1,8 +1,26 @@
 #!/bin/bash -ex
+
+local=false
+while getopts l opt; do
+  case $opt in
+    l )
+      local=true
+      ;;
+    \? )
+      echo "Usage: $0 [-l]"
+      exit 1
+      ;;
+  esac
+done
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 $DIR/bootstrap.sh $DIR $DIR/venv
 
-DISPLAY=:0 $DIR/venv/bin/python $DIR/src/main.py
+if [ "$local" = true ]; then
+  $DIR/venv/bin/python $DIR/src/main.py
+else
+  DISPLAY=:0 $DIR/venv/bin/python $DIR/src/main.py
+fi
 
 exit 0
