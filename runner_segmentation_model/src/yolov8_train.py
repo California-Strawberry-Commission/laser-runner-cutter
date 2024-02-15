4664ddb53fec07f92adc461da7f1f9f0ae9bb3ac
Genki Kondo <kondo.genki@gmail.com>
2024-02-15 09:23:09 -0800

    [runner seg] Add Runner2300 yolov8n-seg model

--- runner_segmentation_model/src/yolov8_train.py
+++ runner_segmentation_model/src/yolov8_train.py
@@ -5,6 +5,7 @@ Description: Script to train a YOLOv8 model
 
 import os
 from ultralytics import settings, YOLO
+from time import perf_counter
 
 project_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 settings.update(
@@ -16,6 +17,7 @@ settings.update(
 )
 
 model = YOLO("yolov8n-seg.yaml")
+train_start = perf_counter()
 results = model.train(
     data=os.path.join(project_path, "dataset.yml"),
     imgsz=(1024, 768),
@@ -24,3 +26,5 @@ results = model.train(
     epochs=150,
     flipud=0.5,
 )
+train_stop = perf_counter()
+print(f"Training finished in {train_stop - train_start} seconds.")
