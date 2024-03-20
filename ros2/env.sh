982be9a641d5fb4cdb310e3f583d39345e9ba32a
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-03-20 13:58:50 -0700

    [ROS][scripts] Shuffle dependencies

--- ros2/env.sh
+++ ros2/env.sh
@@ -1,3 +1,3 @@
-ROS_DEPS="ros-foxy-desktop ros-foxy-diagnostic-updater"
-PYTHON_DEPS="python3-venv python3-pip python3-rosdep2 python3-colcon-common-extensions python3-argcomplete"
+ROS_DEPS="ros-foxy-desktop ros-foxy-diagnostic-updater python3-rosdep2 python3-colcon-common-extensions python3-argcomplete"
+PYTHON_DEPS="python3-venv python3-pip "
 VENV_DIR=~/.ros_venv
