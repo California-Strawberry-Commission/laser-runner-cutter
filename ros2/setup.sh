a519e28ec068e5f252a958f481796b545d72a703
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-03-20 15:45:49 -0700

    [ROS][scripts] remove script from sourceable

--- ros2/setup.sh
+++ ros2/setup.sh
@@ -1,8 +1,9 @@
-script_dir=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
-source $script_dir/env.sh
+set -e
+
+source env.sh
 
 source $VENV_DIR/bin/activate
 source /opt/ros/foxy/setup.sh
-source $script_dir/install/local_setup.sh
+source install/local_setup.sh
 
 export PYTHONPATH=$PYTHONPATH:$VENV_DIR/lib/python3.8/site-packages
