c4a84775526b900cd4c89e49cffa3997955ca022
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-03-25 10:18:59 -0700

    [ROS] Move scripts into dir

---
+++ ros2/scripts/install_realsense_ros.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+source env.sh
+
+# perform all commands in workspace dir
+cd $WS_DIR
+
+# https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md#installing-the-packages
+sudo apt-get install -y apt-transport-https
+
+# Install realsense SDK
+sudo mkdir -p /etc/apt/keyrings
+curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
+sudo tee /etc/apt/sources.list.d/librealsense.list
+sudo apt-get update
+
+sudo apt-get install -y librealsense2-dev librealsense2-dbg
+
+# Install ROS components
+source /opt/ros/foxy/local_setup.bash # Get ROS-specific env
+
+echo "Installing ROS realsense"
+
+# Install dependancies
+sudo apt install -y "ros-$ROS_DISTRO-librealsense2*"
+
+# Init realsense submodule
+git submodule update --init --recursive
+
+# Setup for ROS
+# sudo rosdep init --include-eol-distros      # "sudo rosdep init --include-eol-distros" for Foxy and earlier
+# rosdep update --include-eol-distros         # "sudo rosdep update --include-eol-distros" for Foxy and earlier
