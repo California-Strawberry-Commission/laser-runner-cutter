3ba7e0480cff6525b6cf38cdca28cb1b0b0fde0e
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-03-26 10:50:33 -0700

    [ROS][scripts] Delete old scripts

--- ros2/env.sh
+++
@@ -1,4 +0,0 @@
-ROS_DEPS="ros-foxy-desktop ros-foxy-diagnostic-updater python3-rosdep2 python3-colcon-common-extensions python3-argcomplete"
-PYTHON_DEPS="python3-venv python3-pip"
-VENV_DIR=~/.ros_venv
-TMPDIR=$VENV_DIR/tmp
--- ros2/setup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-source env.sh
-
-source $VENV_DIR/bin/activate
-source install/setup.sh
-
-export PYTHONPATH=$PYTHONPATH:$VENV_DIR/lib/python3.8/site-packages
