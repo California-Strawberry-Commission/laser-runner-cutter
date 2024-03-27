b0158f6b5f19215962e2aa706607c54e4aec1247
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-03-27 12:46:22 -0700

    [ROS][asyncros] Service server impl

--- ros2/amiga_control/amiga_control/amiga_control_node.py
+++ ros2/amiga_control/amiga_control/amiga_control_node.py
@@ -1,7 +1,9 @@
+import asyncio
 from typing import AsyncGenerator
 from amiga_control_interfaces.srv import SetTwist
+from amiga_control_interfaces.action import Run
 from dataclasses import dataclass
-from .ros_asyncio import AsyncNode, action, serve_nodes, service, timer, param, ros_params
+from .ros_asyncio import AsyncNode, action, serve_nodes, service, timer, param, ros_params, rosnode
 from rcl_interfaces.msg import ParameterDescriptor
 
 
@@ -15,8 +17,8 @@ class AmigaParams:
     #     ParameterDescriptor(description="test", read_only=True),
     # )
 
-
-class AmigaControlNode(AsyncNode("amiga_control_node", AmigaParams)):
+@rosnode(AmigaParams)
+class AmigaControlNode(AsyncNode):
     @param(AmigaParams.host)
     async def set_host_param(self, host):
         self.params.host = host
@@ -35,6 +37,7 @@ class AmigaControlNode(AsyncNode("amiga_control_node", AmigaParams)):
     async def act(self, twist) -> AsyncGenerator[int, None]:
         for i in range(10):
             yield i
+            await asyncio.sleep(1)
         return
 
 
@@ -45,16 +48,16 @@ if __name__ == "__main__":
     main()
 
 # CLIENT BELOW HERE
-n = AmigaControlNode().client()
+# n = AmigaControlNode().client()
 
-@n.on("topic")
-async def handler():
-    print("Got message on topic")
+# @n.on("topic")
+# async def handler():
+#     print("Got message on topic")
 
-async def main():
-    await n.set_twist({x: 1, y: 1})
+# async def main():
+#     await n.set_twist({x: 1, y: 1})
     
-    # Problem: async generators don't have return values (...). Important?
-    # There are workarounds but not super pretty.
-    async for progress in n.act({x: 1, y: 1}):
-        print(f"Got progress {progress}")
+#     # Problem: async generators don't have return values (...). Important?
+#     # There are workarounds but not super pretty.
+#     async for progress in n.act({x: 1, y: 1}):
+#         print(f"Got progress {progress}")
--- ros2/amiga_control/amiga_control/ros_asyncio.py
+++ ros2/amiga_control/amiga_control/ros_asyncio.py
@@ -1,5 +1,6 @@
 import asyncio
 import dataclasses
+import re
 from typing import Optional
 import rclpy
 import rclpy.node
@@ -13,6 +14,11 @@ import inspect
 def to_camel_case(snake_str):
     # https://stackoverflow.com/questions/19053707/converting-snake-case-to-lower-camel-case-lowercamelcase
     return "".join(x.capitalize() for x in snake_str.lower().split("_"))
+
+
+def to_snake(camel_str):
+    camel_str = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", camel_str)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", camel_str).lower()
 
 
 async def ros_spin_nodes(nodes):
@@ -33,11 +39,10 @@ async def ros_spin_nodes(nodes):
 def serve_nodes(*nodes):
     rclpy.init()
 
-    node_tasks = []
-    for n in nodes:
-        node_tasks += n._init_as_node()
+    servers = [n.server() for n in nodes]
+    tasks = [task for s in servers for task in s.tasks()]
 
-    tasks = asyncio.wait(node_tasks + [ros_spin_nodes(nodes)])
+    tasks = asyncio.wait(tasks + [ros_spin_nodes(servers)])
     asyncio.get_event_loop().run_until_complete(tasks)
 
 
@@ -60,16 +65,18 @@ def _check_service_function_signature(fn, srv):
             f"    IDL: {srv.__name__} -> \t{idl_params}"
         )
 
+
 def decorate_handler(handler, ros_type, ros_namespace=None, ros_idl=None):
     handler._ros_type = ros_type
     handler._ros_namespace = ros_namespace
     handler._ros_idl = ros_idl
 
+
 # https://stackoverflow.com/questions/11731136/class-method-decorator-with-self-arguments
 def service(namespace, srv_idl):
     def _service(fn):
         _check_service_function_signature(fn, srv_idl)
-        decorate_handler(fn, "service")
+        decorate_handler(fn, "service", ros_idl=srv_idl, ros_namespace=namespace)
         return fn
 
     return _service
@@ -78,7 +85,7 @@ def service(namespace, srv_idl):
 def action(namespace, act_idl):
     def _action(fn):
         _check_service_function_signature(fn, act_idl)
-        decorate_handler(fn, "action")
+        decorate_handler(fn, "action", ros_idl=act_idl, ros_namespace=namespace)
 
         return fn
 
@@ -101,8 +108,146 @@ def param(dataclass_param):
     return _param
 
 
+def rosnode(params_class):
+    def _rosnode(cls):
+        original_init = cls.__init__
+
+        def wrapped_init(self, node_name=to_snake(cls.__name__), params=params_class()):
+            original_init(self, node_name, params)
+
+        cls.__init__ = wrapped_init
+
+        return cls
+
+    return _rosnode
+
+
+class AsyncClientNode(rclpy.node.Node):
+    def __init__(self, async_node):
+        super().__init__(async_node.node_name + "_client")
+        self._n = async_node
+        self._n.params = self._attach_params_dataclass(self._n.params)
+        self._loop = asyncio.get_event_loop()
+
+    def _attach_params_dataclass(self, dataclass):
+        # Declare and update all parameters present in the
+        # passed dataclass.
+        for f in dataclasses.fields(dataclass):
+            self.declare_parameter(f.name, f.default)
+
+            # Map dataclass type to a ROS value attribute
+            # IE int -> get_parameter_value().integer_value
+            getter = getter_map[f.type]
+
+            if getter is None:
+                raise RuntimeError(f"No getter for type {f.type}")
+
+            current_val = getattr(
+                self.get_parameter(f.name).get_parameter_value(), getter
+            )
+            setattr(dataclass, f.name, current_val)
+
+        # TODO: Figure out some kind of notification system for continuous updates
+        # Important: how to notify updates to application code?
+        # self.add_on_set_parameters_callback(self.parameter_callback)
+
+        return dataclass
+
+
+class AsyncServerNode(rclpy.node.Node):
+    def __init__(self, async_node):
+        super().__init__(async_node.node_name)
+        self._n = async_node
+        self._n.params = self._attach_params_dataclass(self._n.params)
+        self._loop = asyncio.get_event_loop()
+
+        # Get ros-asyncio decorated functions to bind.
+        for handler in self._n._get_ros_handlers():
+            print(handler)
+
+    def tasks(self):
+        tasks = []
+
+        for handler in self._n._get_ros_handlers():
+            if handler._ros_type == "service":
+                self._attach_service(handler)
+            else:
+                pass
+                # raise RuntimeError("Unsupported type")
+
+        return tasks
+
+    def _attach_params_dataclass(self, dataclass):
+        # Declare and update all parameters present in the
+        # passed dataclass.
+        for f in dataclasses.fields(dataclass):
+            self.declare_parameter(f.name, f.default)
+
+            # Map dataclass type to a ROS value attribute
+            # IE int -> get_parameter_value().integer_value
+            getter = getter_map[f.type]
+
+            if getter is None:
+                raise RuntimeError(f"No getter for type {f.type}")
+
+            current_val = getattr(
+                self.get_parameter(f.name).get_parameter_value(), getter
+            )
+            setattr(dataclass, f.name, current_val)
+
+        # TODO: Figure out some kind of notification system for continuous updates
+        # Important: how to notify updates to application code?
+        # self.add_on_set_parameters_callback(self.parameter_callback)
+
+        return dataclass
+
+    def _attach_service(self, service_handler):
+        def cb(req, res):
+            print("SERVICE HANDLER START", req)
+            
+            msg_keys = req.get_fields_and_field_types().keys()
+            
+            kwargs = { k:getattr(req, k) for k in msg_keys }
+            
+            result = asyncio.run_coroutine_threadsafe(service_handler(**kwargs), loop=self._loop).result()
+            
+            print("SERVICE HANDLER RUN", result)
+            res.success = result
+            return res
+
+        self.create_service(
+            service_handler._ros_idl, service_handler._ros_namespace, cb
+        )
+
+
 # TODO: Wrap dataclass w/ a setattr trap to trigger notifications
 ros_params = dataclasses.dataclass
+
+
+# Generates a class with passed default values.
+# Syntax sugar to remove need to override __init__
+class AsyncNode:
+    def __init__(
+        self,
+        node_name: str,
+        params: dataclasses.dataclass,
+    ):
+        self.__name__ = to_camel_case(node_name)
+        self.node_name = node_name
+        self.params = params
+
+    def _get_ros_handlers(self):
+        return [
+            getattr(self, d)
+            for d in dir(self)
+            if hasattr(getattr(self, d), "_ros_type")
+        ]
+
+    def server(self):
+        return AsyncServerNode(self)
+
+    def client(self):
+        return AsyncClientNode(self)
 
 
 getter_map = {
@@ -116,79 +261,3 @@ getter_map = {
     "list[float]": "double_array_value",
     "list[str]": "string_array_value",
 }
-
-
-# Generates a class with passed default values.
-# Syntax sugar to remove need to override __init__
-def AsyncNode(node_name, params_class):
-    class _AsyncNodeDefinition(rclpy.node.Node):
-        def __init__(
-            self,
-            node_name: str = node_name,
-            params: dataclasses.dataclass = params_class(),
-        ):
-            # super().__init__(_node_name) # Call LATER after rclpy has initialized.
-            self.__name__ = to_camel_case(node_name)
-            self.node_name = node_name
-            self.params = params_class
-
-        def _get_decorated_handlers(self):
-            return [getattr(self, d) for d in dir(self) if hasattr(getattr(self, d), "_ros_type")]
-
-        def _init_as_node(self):
-            super().__init__(self.node_name)
-            self.params = self._attach_params_dataclass(self.params)
-            
-            # Get ros-asyncio decorated functions to bind. 
-            for handler in self._get_decorated_handlers():
-                print(handler)
-
-            tasks = []
-
-            # Attach services, actions, timers, and param monitors
-            return tasks
-
-        def _init_as_client(self):
-            super().__init__(self.node_name + "_client")
-
-        def _attach_params_dataclass(self, dataclass):
-            # Declare and update all parameters present in the
-            # passed dataclass.
-            for f in dataclasses.fields(dataclass):
-                self.declare_parameter(f.name, f.default)
-
-                # Map dataclass type to a ROS value attribute
-                # IE int -> get_parameter_value().integer_value
-                getter = getter_map[f.type]
-
-                if getter is None:
-                    raise RuntimeError(f"No getter for type {f.type}")
-
-                current_val = getattr(
-                    self.get_parameter(f.name).get_parameter_value(), getter
-                )
-                setattr(dataclass, f.name, current_val)
-
-            # TODO: Figure out some kind of notification system for continuous updates
-            # Important: how to notify updates to application code?
-            # self.add_on_set_parameters_callback(self.parameter_callback)
-
-            return dataclass
-
-        async def _ros_node_task(self):
-            while rclpy.ok():
-                rclpy.spin_once(self, timeout_sec=0)
-
-            await asyncio.sleep(1e-4)
-
-        def run_node(self):
-            asyncio.get_event_loop().run_until_complete(self._ros_node_task())
-
-        def stop(self):
-            _ros_nodes.remove(self)
-
-        # Runs the node
-        async def start():
-            pass
-
-    return _AsyncNode
