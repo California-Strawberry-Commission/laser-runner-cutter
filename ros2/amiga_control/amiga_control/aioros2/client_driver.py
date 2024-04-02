9108e50b0d7dc295baec023f9a1e72abd9761271
DominicChm <45153623+DominicChm@users.noreply.github.com>
2024-04-02 14:44:12 -0700

    [ROS][asyncros] implement action client

--- ros2/amiga_control/amiga_control/aioros2/client_driver.py
+++ ros2/amiga_control/amiga_control/aioros2/client_driver.py
@@ -1,7 +1,39 @@
+import asyncio
 import rclpy
 import rclpy.node
 from .async_driver import AsyncDriver
 from .decorators._decorators import ros_type_e
+from rclpy.action import ActionClient
+from queue import Empty, SimpleQueue
+
+from rclpy.action.client import ClientGoalHandle
+
+class AsyncActionClient:
+    _action_complete = False
+    result = None
+
+    def __init__(self, gen, loop, idl):
+        if gen is None:
+            self._action_complete = True
+            
+        self._gen = gen
+        self._loop = loop
+        self._idl = idl
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if self._action_complete:
+            raise StopAsyncIteration
+        
+        val = await self._loop.run_in_executor(None, self._gen.__next__)
+        
+        if isinstance(val, self._idl.Result):
+            self.result = val
+            raise StopAsyncIteration
+        elif isinstance(val, self._idl.Impl.FeedbackMessage):
+            return val.feedback
 
 class ClientDriver(AsyncDriver):
     def __init__(self, async_node):
@@ -12,7 +44,8 @@ class ClientDriver(AsyncDriver):
 
     def _create_impl(self):
         attachers = {
-            ros_type_e.SERVICE: self._create_service_impl
+            ros_type_e.SERVICE: self._create_service_impl,
+            ros_type_e.ACTION: self._create_action_impl
         }
 
         for handler in self._get_decorated():
@@ -21,6 +54,76 @@ class ClientDriver(AsyncDriver):
             except KeyError:
                 self.log.warn(f"Could not create client implementation for >{handler.__name__}< because type >{handler._ros_type}< is unknown.")
 
+    def _create_action_impl(self, handler):
+        idl = handler._ros_idl
+        
+        def _impl(*args, **kwargs):
+            goal = handler._ros_idl.Goal(*args, **kwargs)
+            generator=self._dispatch_action_req(_impl, goal)
+            return AsyncActionClient(generator, self._loop, handler._ros_idl)
+            
+        
+        _impl._ros_client = ActionClient(self, handler._ros_idl, handler._ros_namespace)
+        setattr(self, handler.__name__, _impl)
+    
+    def _dispatch_action_req(self, impl, request):
+        """Dispatches a ROS action request and yields feedback as it is received
+        side note: wow is the ROS api for this horrible or what???
+        """
+        # https://docs.ros.org/en/foxy/Tutorials/Intermediate/Writing-an-Action-Server-Client/Py.html
+        
+        action_complete = False
+        feedback_queue = SimpleQueue()
+        result = None
+        
+        cli = impl._ros_client
+        
+        # Wait for desired service to become available
+        if not impl._ros_client.wait_for_server(timeout_sec=2.0):
+            self.get_logger().error("Action server not available")
+            return None
+        
+        # Create method-local hooks for callbacks
+        def on_feedback(fb):
+            nonlocal feedback_queue
+            feedback_queue.put(fb)
+            # print("FEEDBACK", fb)
+        
+        def on_result(future):
+            nonlocal action_complete, result
+            result = future.result().result
+            # print("DONE", result)
+            action_complete = True
+            
+        def on_action_response(future):
+            """Entrypoint for initial action response"""
+            goal_handle = future.result()
+            if not goal_handle.accepted:
+                self.log.warn('Goal rejected')
+                return
+
+            print('Goal accepted')
+            
+            # Attach goal callbacks
+            self._get_result_future = goal_handle.get_result_async()
+            self._get_result_future.add_done_callback(on_result)
+        
+        # Request action w/ goal
+        fut = cli.send_goal_async(request, feedback_callback=on_feedback)
+        fut.add_done_callback(on_action_response)
+        
+        # Spin to win
+        while not action_complete:
+            rclpy.spin_once(self)
+            try:
+                yield feedback_queue.get_nowait()
+            except Empty:
+                pass
+                
+        # TODO: yield unyielded feedback?
+        
+        yield result
+        raise StopAsyncIteration()
     
     def _create_service_impl(self, handler):
         async def _impl(*args, **kwargs):
@@ -29,6 +132,8 @@ class ClientDriver(AsyncDriver):
         
         _impl._ros_client = self.create_client(handler._ros_idl, handler._ros_namespace)
         setattr(self, handler.__name__, _impl)
+        
+
     
     def _dispatch_service_req(self, impl, request):
         cli = impl._ros_client
--- ros2/amiga_control/amiga_control/amiga_control_client.py
+++ ros2/amiga_control/amiga_control/amiga_control_client.py
@@ -7,9 +7,20 @@ from common_interfaces.msg import Vector2
 async def _main():
     n = AmigaControlNode().client()
     print("Starting amiga control client")
+    
+    print("Calling service")
     res = await n.set_twist(twist=Vector2(x=1., y=1.))
-    print("Result", res)
+    print("Service Result", res)
+    
+    print("Slow action")
+    async for progress in (action := n.act(fast=False)):
+        print(f"Got progress {progress}")
 
+    print("Fast action")
+    async for feedback in (action := n.act(fast=True)):
+        print(f"Progress {feedback}")
+    print("Result", action.result)
+    
 def main():
     asyncio.run(_main())
 
