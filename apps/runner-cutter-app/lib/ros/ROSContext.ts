55c2ab7ed1ba2f6e432443e93b1d0a9bec5322bc
Genki Kondo <kondo.genki@gmail.com>
2024-04-16 16:06:38 -0700

    [runner cutter app] Use environment variable for rosbridge url

--- apps/runner-cutter-app/lib/ros/ROSContext.ts
+++ apps/runner-cutter-app/lib/ros/ROSContext.ts
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 import ROS from "@/lib/ros/ROS";
 
-const ROSContext = createContext(new ROS("ws://localhost:9090"));
+const ROSContext = createContext(
+  new ROS(process.env.NEXT_PUBLIC_ROSBRIDGE_URL ?? "ws://localhost:9090")
+);
 export default ROSContext;
