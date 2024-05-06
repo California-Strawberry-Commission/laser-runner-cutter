f3d64cf6025f0ca7eab4a1d27689df6e67742278
Genki Kondo <kondo.genki@gmail.com>
2024-05-06 14:57:14 -0700

    [aioros] Simplify spin logic

--- ros2/aioros2/aioros2/aioros2.py
+++ ros2/aioros2/aioros2/aioros2.py
@@ -1,6 +1,6 @@
 import asyncio
 import threading
-from typing import Optional
+from typing import List, Optional
 
 import rclpy
 from rclpy.executors import Executor, MultiThreadedExecutor
@@ -14,48 +14,41 @@ from .server_driver import ServerDriver
 # https://robotics.stackexchange.com/questions/106026/ros2-multi-nodes-each-on-a-thread-in-same-process
 
 
-async def _spin(node: Node, executor: Optional[Executor] = None):
+async def _spin(nodes, num_threads: Optional[int] = None):
     # From https://github.com/mavlink/MAVSDK-Python/issues/419
-    cancel = node.create_guard_condition(lambda: None)
+
+    servers = [ServerDriver(n) for n in nodes]
+    executor = MultiThreadedExecutor(num_threads=num_threads)
+    for node in servers:
+        executor.add_node(node)
+
+    cancels = [node.create_guard_condition(lambda: None) for node in servers]
 
     def spin_inner(
-        node: Node, future: asyncio.Future, event_loop: asyncio.AbstractEventLoop
+        executor: Executor,
+        future: asyncio.Future,
+        event_loop: asyncio.AbstractEventLoop,
     ):
         while not future.cancelled():
-            rclpy.spin_once(node, executor=executor)
+            executor.spin_once()
         if not future.cancelled():
             event_loop.call_soon_threadsafe(future.set_result, None)
 
     event_loop = asyncio.get_event_loop()
     spin_task = event_loop.create_future()
     spin_thread = threading.Thread(
-        target=spin_inner, args=(node, spin_task, event_loop)
+        target=spin_inner, args=(executor, spin_task, event_loop)
     )
     spin_thread.start()
     try:
         await spin_task
     except asyncio.CancelledError:
-        cancel.trigger()
+        for cancel in cancels:
+            cancel.trigger()
     spin_thread.join()
-    node.destroy_guard_condition(cancel)
 
-
-async def _ros_spin_nodes(
-    nodes,
-    num_threads: Optional[int] = None,
-):
-    executor = MultiThreadedExecutor(num_threads=num_threads)
-    servers = [ServerDriver(node) for node in nodes]
-    spin_tasks = [
-        asyncio.get_event_loop().create_task(_spin(server, executor))
-        for server in servers
-    ]
-    await asyncio.wait(spin_tasks, return_when=asyncio.FIRST_COMPLETED)
-
-    # Cancel tasks
-    for task in spin_tasks:
-        if task.cancel():
-            await task
+    for idx, node in enumerate(servers):
+        node.destroy_guard_condition(cancels[idx])
 
 
 def serve_nodes(*nodes, num_threads: Optional[int] = None):
@@ -66,6 +59,6 @@ def serve_nodes(*nodes, num_threads: Optional[int] = None):
 
     rclpy.init()
 
-    asyncio.run(_ros_spin_nodes(nodes, num_threads=num_threads))
+    asyncio.run(_spin(nodes, num_threads=num_threads))
 
     rclpy.shutdown()
