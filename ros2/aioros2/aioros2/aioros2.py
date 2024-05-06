b7e15f4b7222fd01768d878402b41816c815a52e
Genki Kondo <kondo.genki@gmail.com>
2024-05-06 13:01:14 -0700

    [aioros] Fix perf issue for service calls

--- ros2/aioros2/aioros2/aioros2.py
+++ ros2/aioros2/aioros2/aioros2.py
@@ -1,6 +1,9 @@
 import asyncio
+from typing import Optional
+
 import rclpy
 import rclpy.node
+
 from .server_driver import ServerDriver
 
 # pip install -e laser-runner-cutter/ros2/aioros2/ --config-settings editable_mode=strict
@@ -9,10 +12,12 @@ from .server_driver import ServerDriver
 # https://robotics.stackexchange.com/questions/106026/ros2-multi-nodes-each-on-a-thread-in-same-process
 
 
-async def _ros_spin_nodes(nodes, num_threads):
+async def _ros_spin_nodes(
+    nodes,
+    num_threads: Optional[int] = None,
+):
     print("Ros starting up...")
 
-    # TODO: Tune thread counts. Might be limitations - not sure what ROS behavior when running out.
     executor = rclpy.executors.MultiThreadedExecutor(num_threads=num_threads)
 
     servers = [ServerDriver(n) for n in nodes]
@@ -22,17 +27,16 @@ async def _ros_spin_nodes(nodes, num_threads):
 
     print("Ros event loop running!")
     while rclpy.ok():
-        executor.spin_once(timeout_sec=0)
+        executor.spin_once()
         await asyncio.sleep(0)
 
 
-def serve_nodes(*nodes, threads=10):
+def serve_nodes(*nodes, num_threads: Optional[int] = None):
     from .decorators import deferrable_accessor
-    
+
     # Notify deferrables that load has fully completed
     deferrable_accessor.deferrables_frozen = True
 
     rclpy.init()
-    
-    asyncio.run(_ros_spin_nodes(nodes, threads))
-    
+
+    asyncio.run(_ros_spin_nodes(nodes, num_threads=num_threads))
