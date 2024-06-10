5a86629ec3749bdddfb6acb647ec6ca024e32e35
Genki Kondo <kondo.genki@gmail.com>
2024-06-10 20:09:37 +0100

    [ros2] Modify setup.py to copy dirs

--- ros2/camera_control/setup.py
+++ ros2/camera_control/setup.py
@@ -1,25 +1,40 @@
 import os
 from setuptools import setup, find_packages
-from glob import glob
 
 package_name = "camera_control"
+
+
+def package_files(data_files, directory_list):
+    paths_dict = {}
+    for directory in directory_list:
+        for path, directories, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join("share", package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
 
 setup(
     name=package_name,
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
-    data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
-        (
-            os.path.join("share", package_name, "models"),
-            glob(os.path.join("models/**/*.pt"), recursive=True),
-        ),
-        (
-            os.path.join("share", package_name, "calibration_params"),
-            glob(os.path.join("calibration_params/**/*.npy"), recursive=True),
-        ),
-    ],
+    data_files=package_files(
+        [
+            ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+            ("share/" + package_name, ["package.xml"]),
+        ],
+        ["models/", "calibration_params/"],
+    ),
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="genki",
