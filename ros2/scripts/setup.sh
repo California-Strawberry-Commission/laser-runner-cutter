c86011e73fc2058bdde732623369f9f591be1914
Genki Kondo <kondo.genki@gmail.com>
2024-06-17 20:16:27 +0100

    [ros2] Fix setup.sh to source /opt/ros/foxy/setup.sh

--- ros2/scripts/setup.sh
+++ ros2/scripts/setup.sh
@@ -2,6 +2,7 @@ script_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $script_dir/env.sh
 
 source $VENV_DIR/bin/activate
+source /opt/ros/foxy/setup.sh
 source $ROS_WS_DIR/install/setup.sh
 
 export PYTHONPATH=$PYTHONPATH:$VENV_DIR/lib/python3.8/site-packages
