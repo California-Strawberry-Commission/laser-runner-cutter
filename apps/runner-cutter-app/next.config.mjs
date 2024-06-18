d7f598629d45846fcede45be079e7ed74dac1d0e
Genki Kondo <kondo.genki@gmail.com>
2024-06-18 08:54:19 +0100

    [runner cutter app] Enable CORS headers

--- apps/runner-cutter-app/next.config.mjs
+++ apps/runner-cutter-app/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+      return [
+          {
+              // matching all API routes
+              source: "/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "*" },
+                  { key: "Access-Control-Allow-Methods", value: "*" },
+                  { key: "Access-Control-Allow-Headers", value: "*" },
+              ]
+          }
+      ]
+  }
+}
 
 export default nextConfig;
