c89b49672f76591742d8e02e3c6ee1f5e9c41b64
Genki Kondo <kondo.genki@gmail.com>
2024-08-05 20:28:42 +0100

    [runner cutter app] Fix websocket URL for ROS

--- apps/runner-cutter-app/lib/ros/ROSContext.ts
+++ apps/runner-cutter-app/lib/ros/ROSContext.ts
@@ -4,7 +4,8 @@ import ROS from "@/lib/ros/ROS";
 const ROSContext = createContext(
   new ROS(
     typeof window !== "undefined"
-      ? process.env.NEXT_PUBLIC_ROSBRIDGE_URL ?? "ws://${window.location.hostname}:9090"
+      ? process.env.NEXT_PUBLIC_ROSBRIDGE_URL ??
+        `ws://${window.location.hostname}:9090`
       : null
   )
 );
