93a5474a11be2a298b9ce6e43192e138f9137c3f
Genki Kondo <kondo.genki@gmail.com>
2024-09-10 14:27:10 +0100

    [ros2][lifecycle] Run ROS2 reconnect and node monitor tasks each sequentially using setTimeout

---
+++ apps/runner-cutter-app/lib/ros/TaskRunner.ts
@@ -0,0 +1,42 @@
+export default class TaskRunner {
+  private taskFunction: () => any;
+  private intervalMs: number;
+  private cancelExecution: boolean;
+  private isRunning: boolean;
+
+  constructor(taskFunction: () => any, intervalMs: number) {
+    this.taskFunction = taskFunction;
+    this.intervalMs = intervalMs;
+    this.cancelExecution = false;
+    this.isRunning = false;
+  }
+
+  public async start(): Promise<void> {
+    if (this.isRunning) {
+      return;
+    }
+
+    this.cancelExecution = false;
+    this.isRunning = true;
+
+    while (!this.cancelExecution) {
+      try {
+        await this.taskFunction();
+      } catch (error) {
+        console.error("Error during task execution:", error);
+      }
+      await new Promise((resolve) => setTimeout(resolve, this.intervalMs));
+    }
+
+    this.isRunning = false;
+  }
+
+  // Function to cancel the ongoing task execution
+  public stop(): void {
+    if (!this.isRunning) {
+      return;
+    }
+
+    this.cancelExecution = true;
+  }
+}
