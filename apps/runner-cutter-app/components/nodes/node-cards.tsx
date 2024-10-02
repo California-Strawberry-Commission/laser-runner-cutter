a723749bb6439339d182291796cf9ee3c7b50ea8
Genki Kondo <kondo.genki@gmail.com>
2024-10-02 17:43:49 +0100

    [runner cutter app] Click on node status tile to expand and show state

--- apps/runner-cutter-app/components/nodes/node-cards.tsx
+++ apps/runner-cutter-app/components/nodes/node-cards.tsx
@@ -1,39 +1,58 @@
 import type { NodeInfo } from "@/lib/NodeInfo";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 
 function NodeCard({ nodeInfo }: { nodeInfo: NodeInfo }) {
   return (
-    <Card
-      className={cn(
-        "max-w-72",
-        nodeInfo.connected ? "bg-green-500" : "bg-red-500"
-      )}
-    >
-      <CardHeader className="p-4">
-        <CardTitle>{nodeInfo.name}</CardTitle>
-        <CardDescription className="text-foreground">
-          {nodeInfo.connected ? "Connected" : "Disconnected"}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="p-4 pt-0 text-xs">
-        <pre className="overflow-hidden text-ellipsis">
-          {JSON.stringify(nodeInfo.state, undefined, 2)}
+    <Dialog>
+      <DialogTrigger asChild>
+        <Card
+          className={cn(
+            "max-w-48",
+            nodeInfo.connected ? "bg-green-500" : "bg-red-500"
+          )}
+        >
+          <CardHeader className="p-4">
+            <CardTitle className="text-lg overflow-hidden text-ellipsis">
+              {nodeInfo.name}
+            </CardTitle>
+            <CardDescription className="text-foreground">
+              {nodeInfo.connected ? "Connected" : "Disconnected"}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{nodeInfo.name}</DialogTitle>
+          <DialogDescription>
+            {nodeInfo.connected ? "Connected" : "Disconnected"}
+          </DialogDescription>
+        </DialogHeader>
+        <pre className="overflow-hidden text-ellipsis text-xs">
+          {JSON.stringify(nodeInfo.state ?? {}, undefined, 2)}
         </pre>
-      </CardContent>
-    </Card>
+      </DialogContent>
+    </Dialog>
   );
 }
 
 export default function NodeCards({ nodeInfos }: { nodeInfos: NodeInfo[] }) {
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row flex-wrap gap-4 justify-center">
       {nodeInfos.map((nodeInfo) => (
         <NodeCard key={nodeInfo.name} nodeInfo={nodeInfo} />
       ))}
