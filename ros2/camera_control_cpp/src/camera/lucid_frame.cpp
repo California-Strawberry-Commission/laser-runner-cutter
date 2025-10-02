ca471716430c9f160011b58e0e9cdb41a04c1f4f
bholt13 <holt0316@gmail.com>
2025-10-02 16:40:52 -0700

    [ros2][camera_control_cpp] Depth frame intensity added to LucidFrame class members, guard added for constructor

--- ros2/camera_control_cpp/src/camera/lucid_frame.cpp
+++ ros2/camera_control_cpp/src/camera/lucid_frame.cpp
@@ -107,6 +107,7 @@ bool isPixelInLine(const cv::Point2i& curr, const cv::Point2i& start,
 }  // namespace
 
 LucidFrame::LucidFrame(const cv::Mat& colorFrame, const cv::Mat& depthFrameXyz,
+                       const cv::Mat& depthFrameIntensity,
                        double timestampMillis,
                        const cv::Mat& colorCameraIntrinsicMatrix,
                        const cv::Mat& colorCameraDistortionCoeffs,
@@ -125,9 +126,12 @@ LucidFrame::LucidFrame(const cv::Mat& colorFrame, const cv::Mat& depthFrameXyz,
       xyzToColorCameraExtrinsicMatrix_(xyzToColorCameraExtrinsicMatrix),
       xyzToDepthCameraExtrinsicMatrix_(xyzToDepthCameraExtrinsicMatrix),
       colorFrameOffset_(colorFrameOffset) {
-  colorToDepthExtrinsicMatrix_ =
-      xyzToDepthCameraExtrinsicMatrix_ *
-      calibration::invertExtrinsicMatrix(xyzToColorCameraExtrinsicMatrix_);
+  if (!xyzToColorCameraExtrinsicMatrix_.empty() &&
+      !xyzToDepthCameraExtrinsicMatrix_.empty()) {
+    colorToDepthExtrinsicMatrix_ =
+        xyzToDepthCameraExtrinsicMatrix_ *
+        calibration::invertExtrinsicMatrix(xyzToColorCameraExtrinsicMatrix_);
+  }
 }
 
 LucidFrame::~LucidFrame() {}
