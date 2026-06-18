4efb9a2ded8d8d9732f5465e7bb3b1891e80933d
Genki Kondo <kondo.genki@gmail.com>
2026-06-18 18:19:42 +0100

    [ml] Modify install_docker.sh to install NVIDIA Container Toolkit

--- ml/docker/install_docker.sh
+++ ml/docker/install_docker.sh
@@ -29,3 +29,18 @@ fi
 # Add user to the Docker group
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Install the NVIDIA Container Toolkit
+# The following is from https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+sudo apt-get install -y gnupg2
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.19.1-1
+sudo apt-get install -y \
+    nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+    nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+    libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+    libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
