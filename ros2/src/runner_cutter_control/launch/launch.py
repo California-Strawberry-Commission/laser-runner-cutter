9c27fd42549a430ba6bee675b0d53c8b3451ca65
bholt13 <holt0316@gmail.com>
2026-09-21 15:03:06 -0700

    [runner_cutter_control][launch/launch.py] Modify to use OpaquFunction, a type of action, versus a general action to load components.

--- ros2/src/runner_cutter_control/launch/launch.py
+++ ros2/src/runner_cutter_control/launch/launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler, TimerAction
+from launch.actions import OpaqueFunction, RegisterEventHandler, TimerAction
 from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
@@ -31,56 +31,63 @@ def generate_launch_description():
     # Therefore, components are loaded with OnProcessStart rather than an inline description.
     # The event handler fires on every process start, including respawns.
     # See: https://github.com/ros2/launch_ros/issues/361
-    load_camera_detection = LoadComposableNodes(
-        target_container="camera_detection_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="camera_control",
-                plugin="CameraControlNode",
-                name="camera0",
-                parameters=[parameters_file],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            ),
-            ComposableNode(
-                package="detection",
-                plugin="DetectionNode",
-                name="detection0",
-                parameters=[parameters_file],
-                extra_arguments=[{"use_intra_process_comms": True}],
-                remappings=[
-                    (
-                        "color/image_raw",
-                        "/camera0/color/image_raw",
-                    ),  # sub, raw color camera image
-                    (
-                        "color/camera_info",
-                        "/camera0/color/camera_info",
-                    ),  # sub, color camera info
-                    (
-                        "depth/xyz",
-                        "/camera0/depth/xyz",
-                    ),  # sub, depth xyz data
-                    (
-                        "depth/camera_info",
-                        "/camera0/depth/camera_info",
-                    ),  # sub, depth camera info
-                    (
-                        "debug/image",
-                        "/detection0/debug/image",
-                    ),  # pub, color debug image
-                    (
-                        "debug/depth_image",
-                        "/detection0/debug/depth_image",
-                    ),  # pub, depth debug image
+    def load_camera_detection(context, *args, **kwargs):
+        return [
+            LoadComposableNodes(
+                target_container="camera_detection_container",
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package="camera_control",
+                        plugin="CameraControlNode",
+                        name="camera0",
+                        parameters=[parameters_file],
+                        extra_arguments=[{"use_intra_process_comms": True}],
+                    ),
+                    ComposableNode(
+                        package="detection",
+                        plugin="DetectionNode",
+                        name="detection0",
+                        parameters=[parameters_file],
+                        extra_arguments=[{"use_intra_process_comms": True}],
+                        remappings=[
+                            (
+                                "color/image_raw",
+                                "/camera0/color/image_raw",
+                            ),  # sub, raw color camera image
+                            (
+                                "color/camera_info",
+                                "/camera0/color/camera_info",
+                            ),  # sub, color camera info
+                            (
+                                "depth/xyz",
+                                "/camera0/depth/xyz",
+                            ),  # sub, depth xyz data
+                            (
+                                "depth/camera_info",
+                                "/camera0/depth/camera_info",
+                            ),  # sub, depth camera info
+                            (
+                                "debug/image",
+                                "/detection0/debug/image",
+                            ),  # pub, color debug image
+                            (
+                                "debug/depth_image",
+                                "/detection0/debug/depth_image",
+                            ),  # pub, depth debug image
+                        ],
+                    ),
                 ],
-            ),
-        ],
-    )
+            )
+        ]
 
     load_camera_detection_on_start = RegisterEventHandler(
         OnProcessStart(
             target_action=camera_detection_launch_node,
-            on_start=[TimerAction(period=1.0, actions=[load_camera_detection])],
+            on_start=[
+                TimerAction(
+                    period=1.0, actions=[OpaqueFunction(function=load_camera_detection)]
+                )
+            ],
         )
     )
 
